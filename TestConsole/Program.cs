093e506be47a4b3505da7564752ab517a6e3ae19
Corinna <corinna.zwinge@rwth-aachen.de>
2024-02-06 17:40:27 +0100

    update

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -4,28 +4,34 @@ using CollectionHandling;
 using Data;
 using System.Text.Json;
 
+string br = "---------------------------------------";
 Console.WriteLine("Hello, World!");
 
 HttpCall myCall = new HttpCall();
 
 Card cd = new Card();
 cd = await myCall.GetCardDataById("23002");
-//Console.WriteLine(cd.text);
+Console.WriteLine(cd.text);
+Console.WriteLine(br);
 
 Deck deck = new Deck();
 deck = await myCall.GetDeckById("28184");
-//Console.WriteLine(deck.name);
+Console.WriteLine(deck.name);
+Console.WriteLine(br);
 
 List<Card> cards = new List<Card>();
 cards = await myCall.GetAllCards();
-//Console.WriteLine(cards.Count);
+Console.WriteLine(cards.Count);
+Console.WriteLine(br);
 
 List<Pack> packs = new List<Pack>();
 packs = await myCall.GetAllPacks();
-//Console.WriteLine(packs.Count);
+Console.WriteLine(packs.Count);
+Console.WriteLine(br);
 
 cards = await myCall.GetCardsFromPackByCode("Core");
-//Console.WriteLine(cards[0].name);
+Console.WriteLine(cards[0].name);
+Console.WriteLine(br);
 
 Console.WriteLine("-------------------------------");
 CollectionHandler handler = new CollectionHandler();
